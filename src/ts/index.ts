71fba2e4144df92ac3313bf7aa493469cc9fc64f
JUNKI555 <junki555@gmail.com>
2020-11-18 16:28:57 +0900

    :construction: add sample source umbrellajs

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,8 +1,12 @@
-import { addBaz } from './baz'
+import { addBaz } from "./baz"
+import u from "umbrella";
 
 window.onload = function () {
   const app = document.getElementById("foo") as HTMLParagraphElement;
   
   app.innerHTML ="Hello Typescript World! foo";
   addBaz(app);
+
+  u<HTMLParagraphElement>("#foo").addClass('main');
+  console.log("#foo nodes:", u<HTMLParagraphElement>("#foo").length);
 };
